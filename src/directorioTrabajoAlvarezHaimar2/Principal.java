5efe19ce78020cc3b11a4bc6724a4f1923e7b2eb
unknown <DAM101@iesataulfo.local>
2024-05-14 16:52:47 +0200

    Fase 2 Clase Principal comentada

--- src/directorioTrabajoAlvarezHaimar2/Principal.java
+++ src/directorioTrabajoAlvarezHaimar2/Principal.java
@@ -2,8 +2,9 @@ package directorioTrabajoAlvarezHaimar2;
 
 public class Principal {
 
+	//Creamos el metodo main se encargara de crear un objeto de la clase Clase
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	//Creamos el objeto de la clase Clase
 
 	}
 
