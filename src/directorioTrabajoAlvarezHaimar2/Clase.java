d4d712300b4a3d6e7961c9a0e30de5c71dd10128
unknown <DAM101@iesataulfo.local>
2024-05-14 16:56:26 +0200

    Fase 4 clase Clase comentada y codificada

--- src/directorioTrabajoAlvarezHaimar2/Clase.java
+++ src/directorioTrabajoAlvarezHaimar2/Clase.java
@@ -3,5 +3,7 @@ package directorioTrabajoAlvarezHaimar2;
 public class Clase {
 
 	//Contructor del objeto de la clase Clase
-	
+	public Clase() {
+		System.out.println("Soy un objeto de la clase y acabo de ser creado");
+	}
 }
