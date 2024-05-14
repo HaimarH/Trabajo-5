819ea94aee3a53c01568076d7ff2e9c1630df6b1
unknown <DAM101@iesataulfo.local>
2024-05-14 16:57:34 +0200

    Fase 5 Ambas clases comentadas y codificadas

--- src/directorioTrabajoAlvarezHaimar2/Principal.java
+++ src/directorioTrabajoAlvarezHaimar2/Principal.java
@@ -5,7 +5,7 @@ public class Principal {
 	//Creamos el metodo main se encargara de crear un objeto de la clase Clase
 	public static void main(String[] args) {
 	//Creamos el objeto de la clase Clase
-
+		Clase clase=new Clase();
 	}
 
 }
